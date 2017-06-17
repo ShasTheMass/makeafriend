78fab39de0422e01f42ceca473b5666e1e5e91a9
Shas <shaswarbaban@gmail.com>
2017-06-17 19:11:42 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+sports = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+          [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+          [1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+          [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+          [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+          [0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1],
+          [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]]
+
+
+column_name = list
+
+user_names = list
+
+df = pd.DataFrame(sports, index=user_names, columns=column_name)
+
+new_student = pd.Series()
+
+new user profile
+
+def most_similar_friend(new_student, all_students):
+    np.sort(np.sum(np.vdot(new_student)))
+create function for similarity checking
+output most similar student
+
+
+assume a chat bot
+
+output of the chat bot is to introduce a common interest between the new student and the most similar student
+
+give extrat points to the most social students
+
+
